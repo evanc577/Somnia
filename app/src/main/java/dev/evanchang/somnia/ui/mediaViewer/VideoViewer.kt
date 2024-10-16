c7261cc76b29f188437c714c852c1c280ff5c949
Evan Chang <evanc577@gmail.com>
2024-10-15 21:14:33 -0400

    VideoViewer set immersive mode

--- app/src/main/java/dev/evanchang/somnia/ui/mediaViewer/VideoViewer.kt
+++ app/src/main/java/dev/evanchang/somnia/ui/mediaViewer/VideoViewer.kt
@@ -1,11 +1,16 @@
 package dev.evanchang.somnia.ui.mediaViewer
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -13,9 +18,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
@@ -23,12 +33,15 @@ import androidx.media3.ui.PlayerView
 @OptIn(UnstableApi::class)
 @Composable
 fun VideoViewer(mediaItem: MediaItem) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
     // Save current playback state in case screen rotates and configuration is lost
     var isPlaying by rememberSaveable { mutableStateOf(true) }
     var currentPosition by rememberSaveable { mutableStateOf(0L) }
+    var immersive by rememberSaveable { mutableStateOf(true) }
 
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
+    // Create player
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
             setMediaItem(mediaItem)
@@ -45,13 +58,37 @@ fun VideoViewer(mediaItem: MediaItem) {
             useController = true
             setShowNextButton(false)
             setShowPreviousButton(false)
-            setShowFastForwardButton(false)
-            setShowRewindButton(false)
+            setControllerVisibilityListener(object : PlayerView.ControllerVisibilityListener {
+                // Set immersive variable depending on controller visibility
+                override fun onVisibilityChanged(visibility: Int) {
+                    if (isControllerFullyVisible) {
+                        immersive = false
+                    } else {
+                        immersive = true
+                    }
+                }
+            })
             FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
             )
         }
     })
+
+    // Toggle immersive mode
+    val view = LocalView.current
+    LaunchedEffect(immersive) {
+        if (immersive) {
+            view.windowInsetsController?.hide(
+                android.view.WindowInsets.Type.systemBars()
+            )
+        } else {
+            view.windowInsetsController?.show(
+                android.view.WindowInsets.Type.systemBars()
+            )
+        }
+    }
+
+    // Clean up player
     DisposableEffect(lifecycleOwner) {
         onDispose {
             isPlaying = exoPlayer.isPlaying
