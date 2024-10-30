e41c005b029e6d619890e76637892b39c30bee62
Evan Chang <evanc577@gmail.com>
2024-10-30 02:36:52 -0400

    Adjust bottom sheets icons

--- app/src/main/java/dev/evanchang/somnia/ui/util/BottomSheetGridItem.kt
+++ app/src/main/java/dev/evanchang/somnia/ui/util/BottomSheetGridItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,14 +28,20 @@ fun BottomSheetGridItem(
     label: String,
     onClick: () -> Unit,
 ) {
-    Box(modifier = Modifier.padding(16.dp)) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.clickable { onClick() }) {
+    Box(modifier = Modifier.clickable { onClick() }, contentAlignment = Alignment.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp),
+        ) {
             Icon(
                 imageVector = icon, contentDescription = label, modifier = Modifier.size(52.dp)
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = label, textAlign = TextAlign.Center)
+            Text(
+                text = label,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.labelMedium,
+            )
         }
     }
 }
