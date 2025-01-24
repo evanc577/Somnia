52579a1b3c4831489b14fb638f38ff956ebd9b3d
Evan Chang <evanc577@gmail.com>
2025-01-24 15:50:30 -0500

    Cache scraped Imgur client ID

--- app/src/main/java/dev/evanchang/somnia/api/media/Imgur.kt
+++ app/src/main/java/dev/evanchang/somnia/api/media/Imgur.kt
@@ -10,11 +10,15 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.URLProtocol
 import io.ktor.http.appendPathSegments
+import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
@@ -31,57 +35,54 @@ class Imgur {
     }
 
     private suspend fun getClientId(): ApiResult<String> {
-        // Get main.js
-        val htmlResponse = doRequest<String> {
-            ImgurClient.client.get {
-                url {
-                    protocol = this@Imgur.protocol
-                    host = "imgur.com"
+        ImgurClient.mutex.withLock {
+            if (ImgurClient.cachedClientId != null) {
+                return ApiResult.Ok(ImgurClient.cachedClientId!!)
+            }
+
+            // Get main.js
+            val htmlResponse = doRequest<String> {
+                ImgurClient.client.get {
+                    url {
+                        protocol = this@Imgur.protocol
+                        host = "imgur.com"
+                    }
                 }
             }
-        }
-        val htmlText = when (htmlResponse) {
-            is ApiResult.Ok -> htmlResponse.value
-            is ApiResult.Err -> return htmlResponse
-        }
-        val m = ImgurClient.mainJsRe.find(htmlText) ?: return ApiResult.Err("main.js not found")
-        val jsUrl = m.value
+            val htmlText = when (htmlResponse) {
+                is ApiResult.Ok -> htmlResponse.value
+                is ApiResult.Err -> return htmlResponse
+            }
+            val m = ImgurClient.mainJsRe.find(htmlText) ?: return ApiResult.Err("main.js not found")
+            val jsUrl = m.value
 
-        // Get client ID
-        val jsResponse = doRequest<String> {
-            ImgurClient.client.get(jsUrl)
+            // Get client ID
+            val jsResponse = doRequest<String> {
+                ImgurClient.client.get(jsUrl)
+            }
+            val jsData = when (jsResponse) {
+                is ApiResult.Ok -> jsResponse.value
+                is ApiResult.Err -> return jsResponse
+            }
+            val mClientId =
+                ImgurClient.clientIdRe.find(jsData) ?: return ApiResult.Err("client id not found")
+            val clientId = mClientId.groups[1]!!.value
+
+            // Cache this client ID
+            ImgurClient.cachedClientId = clientId
+
+            return ApiResult.Ok(clientId)
         }
-        val jsData = when (jsResponse) {
-            is ApiResult.Ok -> jsResponse.value
-            is ApiResult.Err -> return jsResponse
-        }
-        val mClientId =
-            ImgurClient.clientIdRe.find(jsData) ?: return ApiResult.Err("client id not found")
-        return ApiResult.Ok(mClientId.groups[1]!!.value)
     }
 
     private suspend fun callApi(
         id: String, apiType: ImgurApiType
     ): ApiResult<ImmutableList<MediaItem>> {
-        val clientId = when (val c = getClientId()) {
-            is ApiResult.Ok -> c.value
-            is ApiResult.Err -> return c
-        }
 
         val apiResponse = doRequest<ImgurAlbumResponse> {
-            ImgurClient.client.get {
-                url {
-                    protocol = this@Imgur.protocol
-                    host = "api.imgur.com"
-                    appendPathSegments("post", "v1")
-                    when (apiType) {
-                        is ImgurApiType.Album -> appendPathSegments("albums")
-                        is ImgurApiType.Media -> appendPathSegments("media")
-                    }
-                    appendPathSegments(id)
-                    parameters.append("client_id", clientId)
-                    parameters.append("include", "media")
-                }
+            when (val apiHelperResponse = callApiHelper(id, apiType)) {
+                is ApiResult.Err -> return apiHelperResponse
+                is ApiResult.Ok -> apiHelperResponse.value
             }
         }
 
@@ -103,6 +104,41 @@ class Imgur {
                 height = media.height
             )
         }.toImmutableList())
+    }
+
+    private suspend fun callApiHelper(id: String, apiType: ImgurApiType): ApiResult<HttpResponse> {
+        for (i in 0..1) {
+            val clientId = when (val c = getClientId()) {
+                is ApiResult.Ok -> c.value
+                is ApiResult.Err -> return c
+            }
+
+            val resp = ImgurClient.client.get {
+                url {
+                    protocol = this@Imgur.protocol
+                    host = "api.imgur.com"
+                    appendPathSegments("post", "v1")
+                    when (apiType) {
+                        is ImgurApiType.Album -> appendPathSegments("albums")
+                        is ImgurApiType.Media -> appendPathSegments("media")
+                    }
+                    appendPathSegments(id)
+                    parameters.append("client_id", clientId)
+                    parameters.append("include", "media")
+                }
+            }
+
+            if (resp.status.isSuccess()) {
+                return ApiResult.Ok(resp)
+            }
+
+            // Try refreshing client ID again if failed
+            ImgurClient.mutex.withLock {
+                ImgurClient.cachedClientId = null
+            }
+        }
+
+        return ApiResult.Err("Imgur API call failed")
     }
 }
 
@@ -128,8 +164,13 @@ private object ImgurClient {
         }
     }
 
+    // Regexes for extracting client ID from website
     val mainJsRe = Regex("https?://s\\.imgur\\.com/desktop-assets/js/main\\.\\w+\\.js")
     val clientIdRe = Regex("apiClientId: *\"(\\w+)")
+
+    // Cached client ID
+    var cachedClientId: String? = null
+    val mutex = Mutex()
 }
 
 @Serializable
